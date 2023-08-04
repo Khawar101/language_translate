095599815b486950d155484a0f8d4d28b60b8dec
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-08-04 15:33:34 +0500

    pop up menu

--- lib/post_now.dart
+++ lib/post_now.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PostNowData extends StatefulWidget {
   const PostNowData({super.key});
@@ -16,6 +17,7 @@ class _PostNowDataState extends State<PostNowData> {
   // final items = [
   //   'Item 1',
   //   'Item 2',
+
   //   'Item 3',
   //   'Item 4',
   //   'Item 5',
@@ -53,22 +55,21 @@ class _PostNowDataState extends State<PostNowData> {
                           itemBuilder: (context, index) {
                             //          Map<String, dynamic> data =
                             // document.data()! as Map<String, dynamic>;
-                            var data=snapshot.data!.docs[index];
+                            var data = snapshot.data!.docs[index];
                             return ListTile(
                               horizontalTitleGap: 7,
                               contentPadding: const EdgeInsets.all(0),
                               minVerticalPadding: 0,
-                              title:
-                                  Text(data['email'].toString()),
-                              leading:  CircleAvatar(
+                              title: Text(data['email'].toString()),
+                              leading: CircleAvatar(
                                 radius: 25,
                                 backgroundImage:
                                     NetworkImage(data['profile'].toString()),
                                 backgroundColor: Colors.red,
                                 foregroundColor: Colors.transparent,
                               ),
-                              subtitle:  Text(data['password'].toString()),
-                              trailing: const Icon(Icons.edit),
+                              subtitle: Text(data['password'].toString()),
+                              trailing: const PopupMenu(),
                             );
                           });
                     })
@@ -77,6 +78,51 @@ class _PostNowDataState extends State<PostNowData> {
           ),
         ),
       ),
+    );
+  }
+}
+
+enum SampleItem { itemOne, itemTwo }
+
+class PopupMenu extends StatefulWidget {
+  const PopupMenu({super.key});
+
+  @override
+  State<PopupMenu> createState() => _PopupMenuState();
+}
+
+class _PopupMenuState extends State<PopupMenu> {
+  SampleItem? selectedMenu;
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton<SampleItem>(
+      // padding: const EdgeInsets.all(0),
+      iconSize: 15, position: PopupMenuPosition.over,
+      initialValue: selectedMenu,
+
+      // Callback that sets the selected popup menu item.
+      onSelected: (SampleItem item) {
+        setState(() {
+          selectedMenu = item;
+        });
+      },
+      itemBuilder: (BuildContext context) => <PopupMenuEntry<SampleItem>>[
+        PopupMenuItem<SampleItem>(
+            //  padding: EdgeInsets.all(0),
+            value: SampleItem.itemOne,
+            child: Text(
+              "Delete",
+              style: GoogleFonts.ibmPlexSans(color: Colors.red),
+            )),
+        PopupMenuItem<SampleItem>(
+            value: SampleItem.itemTwo,
+            // padding: EdgeInsets.all(0),
+            child: Text(
+              "Edit",
+              style: GoogleFonts.ibmPlexSans(color: Colors.black),
+            ))
+      ],
     );
   }
 }
