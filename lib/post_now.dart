d9e98f9beb8543a04b8fec56556c8936501b703d
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-08-04 12:16:19 +0500

    post now data

--- lib/post_now.dart
+++ lib/post_now.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class PostNowData extends StatefulWidget {
@@ -8,17 +9,21 @@ class PostNowData extends StatefulWidget {
 }
 
 class _PostNowDataState extends State<PostNowData> {
-  final items = [
-    'Item 1',
-    'Item 2',
-    'Item 3',
-    'Item 4',
-    'Item 5',
-    'Item 6',
-    'Item 7',
-    'Item 8',
-    'Item 9',
-  ];
+  Stream collectionStream =
+      FirebaseFirestore.instance.collection('users').snapshots();
+  final Stream<QuerySnapshot> _usersStream =
+      FirebaseFirestore.instance.collection('users').snapshots();
+  // final items = [
+  //   'Item 1',
+  //   'Item 2',
+  //   'Item 3',
+  //   'Item 4',
+  //   'Item 5',
+  //   'Item 6',
+  //   'Item 7',
+  //   'Item 8',
+  //   'Item 9',
+  // ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,30 +34,44 @@ class _PostNowDataState extends State<PostNowData> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                ListView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount: items.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                       horizontalTitleGap:7,
-                       
-                      contentPadding: const EdgeInsets.all(0),
-                      minVerticalPadding: 0,
-                      title: Text(items[index]),
-                      leading: 
-                      const CircleAvatar(
-                        radius: 25,
-                        backgroundImage:
-                            AssetImage('assets/images/food-image.jpeg'),
-                        backgroundColor: Colors.red,
-                        foregroundColor: Colors.transparent,
-                      ),
-                      subtitle: const Text("email or password"),
-                      trailing: const Icon(Icons.edit),
-                    );
-                  },
-                )
+                StreamBuilder<QuerySnapshot>(
+                    stream: _usersStream,
+                    builder: (BuildContext context,
+                        AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (snapshot.hasError) {
+                        return const Text('Something went wrong');
+                      }
+
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Text("Loading");
+                      }
+
+                      return ListView.builder(
+                          physics: const NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          itemCount: snapshot.data!.docs.length,
+                          itemBuilder: (context, index) {
+                            //          Map<String, dynamic> data =
+                            // document.data()! as Map<String, dynamic>;
+                            var data=snapshot.data!.docs[index];
+                            return ListTile(
+                              horizontalTitleGap: 7,
+                              contentPadding: const EdgeInsets.all(0),
+                              minVerticalPadding: 0,
+                              title:
+                                  Text(data['email'].toString()),
+                              leading:  CircleAvatar(
+                                radius: 25,
+                                backgroundImage:
+                                    NetworkImage(data['profile'].toString()),
+                                backgroundColor: Colors.red,
+                                foregroundColor: Colors.transparent,
+                              ),
+                              subtitle:  Text(data['password'].toString()),
+                              trailing: const Icon(Icons.edit),
+                            );
+                          });
+                    })
               ],
             ),
           ),
