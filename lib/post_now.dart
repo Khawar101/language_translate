3c3be3b7b52a720890cfbd20b52272903c91c116
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-08-07 19:15:49 +0500

    .

--- lib/post_now.dart
+++ lib/post_now.dart
@@ -2,7 +2,6 @@ import 'dart:developer';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 import 'img_picker.dart';
 
 class PostNowData extends StatefulWidget {
