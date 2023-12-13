ddffbf845c83c3fdd7fdd59fcd48f8b57a4ca827
Khawar101 <khawarjutt101@gmail.com>
2023-12-13 10:11:17 +0500

    image picker

--- lib/image picker2.dart
+++ lib/image picker2.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:image_picker_platform_interface/image_picker_platform_interface.dart';
 import 'dart:async';
 import 'dart:io';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:mime/mime.dart';
 import 'package:video_player/video_player.dart';
